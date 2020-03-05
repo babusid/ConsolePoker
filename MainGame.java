bb265814176b414be39bf697f7fc0294ab949ac7
babusid <sidharth.n.babu@gmail.com>
2020-03-05 09:36:16 -0600

    Restructured Testing Code

--- MainGame.java
+++ MainGame.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 public class MainGame {
     public static void main(String[] args){
-        Deck test = new Deck();
+        Deck test = new Deck(52);
         //System.out.println("Testing Cards: "+(new Card())+", "+(new Card())+", "+(new Card())+", "+(new Card())+", ");
         System.out.println("Flop: "+Arrays.toString(test.getFlop()));
         System.out.println("Turn: "+test.getTurn());
         System.out.println("River: "+test.getRiver()+"\n");
         System.out.println("All Used Cards");
-        test.getCard(52).printUsedCards();
+        test.getCard(51).printUsedCards();
 
     }
 }
